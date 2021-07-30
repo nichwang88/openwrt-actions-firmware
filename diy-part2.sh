60177e028ed71f67d2bd7795ab473274851d318c
nichwang88 <nic.hwang@hotmail.com>
2021-07-30 22:18:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+echo '修改网关地址'
+sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
